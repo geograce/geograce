de292dcd85bc39772b750f3e6057608394937988
geograce <klogdui@gmail.com>
2020-05-30 22:13:21 +0800

    0.0.1

---
+++ dictionary/dictionary_a.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta name="description" content="Геодезия алфавит а">
+   <link rel="stylesheet" href="/css/style.css">
+   <link rel="stylesheet" href="/css/media.css">
+   <link rel="icon" href="/img/general/logo_geograce.svg">
+   <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,700&display=swap" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:300&display=swap" rel="stylesheet">
+   <title>Алфавит А</title>
+</head>
+
+<body>
+   <?php
+      require "../module_php/menu_general.php";
+   ?>
+
+   <div class="box_dictionary_general">
+      <p class="title_dictionary">А</p>
+      <a href="/dictionary/a/aberratsiya-astronomicheskaya.php">Аберрация астрономическая</a>
+      <a href="/dictionary/a/abris.php">Абрис</a>
+      <a href="/dictionary/a/additamenty.php">Аддитаменты</a>
+      <a href="/dictionary/a/azimut-astronomicheskiy.php">Азимут астрономический (истинный)</a>
+      <a href="/dictionary/a/azimut-geodezicheskiy.php">Азимут геодезический</a>
+      <a href="/dictionary/a/azimut-magnitnyy.php">Азимут магнитный</a>
+      <a href="/dictionary/a/azimut-pryamogo-normalnogo-secheniya.php">Азимут прямого нормального сечения</a>
+      <a href="/dictionary/a/algoritm.php">Алгоритм</a>
+      <a href="/dictionary/a/almukantarat.php">Альмукантарат</a>
+      <a href="/dictionary/a/anaglifov-tsvetnykh-metod.php">Анаглифов цветных метод</a>
+      <a href="/dictionary/a/anomaliya-sily-tyazhesti.php">Анамалия силы тяжести</a>
+      <a href="/dictionary/a/atlasy-geograficheskiye.php">Атласы географические</a>
+      <a href="/dictionary/a/aeronivelirovaniye.php">Аэронивелирование</a>
+      <a href="/dictionary/a/aerofotoapparat.php">Аэрофотоаппарат</a>
+      <a href="/dictionary/a/aerofotosnimok.php">Аэрофотоснимок</a>
+      <a href="/dictionary/a/aerofotosyemka.php">Аэрофотосъемка</a>
+      <a href="/dictionary/a/aerofototopograficheskaya-syemka.php">Аэрофототопографическая съемка</a>
+      <a href="/dictionary/a/aerofototopografiya.php">Аэрофототопография</a>
+   </div>
+
+   <?php
+      require "../module_php/footer_general.php";
+   ?>
+
+   <script src="/js/mobile_menu_general.js"></script>
+</body>
+
+</html>
