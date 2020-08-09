e5141bb83559c3e1fe85f08d9e3ae1df52a39cff
geograce <geograce@yandex.ru>
2020-08-10 00:54:13 +0800

    2.1.5
    • Изменено меню главной страницы
    • Изменен заголовок H1 на главной странице
    • Скорректирован title и description главной страницы
    • Изменен способ верификации в google на meta тег
    • Переработано мобильное меню на главной странице

--- js/mobile_menu_index.js
+++ js/mobile_menu_index.js
@@ -1,22 +1,36 @@
-//Мобильное меню RIGHT INDEX===============================
-let bgRightMenuIndex = document.querySelector("#bg_right_menu_index");
-let mobileRightMenuIndex = document.querySelector(".mobile_right_menu_index");
-let closeMobileRightMenuIndex = document.querySelector(".close_mobile_right_menu_index");
-
-bgRightMenuIndex.onclick = function(){
-   mobileRightMenuIndex.style.display = "flex";
-}
-closeMobileRightMenuIndex.onclick = function(){
-   mobileRightMenuIndex.style.display = "none";
-}
-//Мобильное меню LEFT INDEX================================
-let bgLeftMenuIndex = document.querySelector("#bg_left_menu_index");
+//ЛЕВОЕ МЕНЮ=================
 let mobileLeftMenuIndex = document.querySelector(".mobile_left_menu_index");
+let boxMenuIndex = document.querySelector(".box_menu_index");
 let closeMobileLeftMenuIndex = document.querySelector(".close_mobile_left_menu_index");
+let leftHeaderAlphabet = document.querySelector("#left_header_alphabet");
 
-bgLeftMenuIndex.onclick = function(){
+boxMenuIndex.onclick = function(){
    mobileLeftMenuIndex.style.display = "flex";
+}
+leftHeaderAlphabet.onclick = function(){
+   boxAlphabet.style.display = "grid";
+   mobileLeftMenuIndex.style.display = "none";
 }
 closeMobileLeftMenuIndex.onclick = function(){
    mobileLeftMenuIndex.style.display = "none";
 }
+
+
+//ПРАВОЕ МЕНЮ================
+let boxAlphabet = document.querySelector(".box_alphabet");
+let rightHeaderAlphabet = document.querySelector("#right_header_alphabet");
+let closeAlphabetMenuIndex = document.querySelector(".close_alphabet_menu_index");
+
+rightHeaderAlphabet.onclick = function(){
+   boxAlphabet.style.display = "grid";
+}
+closeAlphabetMenuIndex.onclick = function(){
+   boxAlphabet.style.display = "none";
+}
+
+// rightHeaderAlphabet.onclick = function(){
+//    boxAlphabet.classList.add("demo");
+// }
+// closeAlphabetMenuIndex.onclick = function(){
+//    boxAlphabet.classList.remove("demo");
+// }
